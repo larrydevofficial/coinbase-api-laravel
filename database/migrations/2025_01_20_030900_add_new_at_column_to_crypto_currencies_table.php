8691405a0387325988887d611179f993ec02eef7
Larry Herrod <lhherrod@gmail.com>
2025-01-19 21:24:29 -0600

    add drop column to new at column migration

--- database/migrations/2025_01_20_030900_add_new_at_column_to_crypto_currencies_table.php
+++ database/migrations/2025_01_20_030900_add_new_at_column_to_crypto_currencies_table.php
@@ -22,7 +22,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('crypto_currencies', function (Blueprint $table) {
-            //
+            $table->dropColumn('new_at');
         });
     }
 };
